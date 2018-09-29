dad0eed78afd3af617ff1280b692b54e0ad72888
mirgee <mrslvkvr@gmail.com>
2018-09-29 12:01:59 +0200

    Added logistic regression classifier

--- src/config.py
+++ src/config.py
@@ -8,6 +8,8 @@ DATA_ROOT = os.path.abspath(os.path.join(PROJ_ROOT, 'data'))
 RAW_ROOT = os.path.abspath(os.path.join(DATA_ROOT, 'raw'))
 PROCESSED_ROOT = os.path.abspath(os.path.join(DATA_ROOT, 'processed'))
 LABELED_ROOT = os.path.abspath(os.path.join(DATA_ROOT, 'labeled'))
+VISUAL_ROOT = os.path.abspath(os.path.join(DATA_ROOT, 'visual'))
+
 
 CHANNEL_NAMES = ['FP1', 'FP2', 'F3', 'F4', 'C3', 'C4', 'P3', 'P4', 'O1', 'O2',
                  'F7', 'F8', 'T3', 'T4', 'T5', 'T6', 'Fz', 'Cz', 'Pz']
