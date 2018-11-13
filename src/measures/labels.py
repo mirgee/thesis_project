a5de031ad0d29649e22286aa12a3f27b60db2ae0
mirgee <mrslvkvr@gmail.com>
2018-11-13 13:18:20 +0100

    Add visualization of distances

--- src/measures/labels.py
+++ src/measures/labels.py
@@ -4,8 +4,8 @@ import os
 import pandas as pd
 
 from config import LABELED_ROOT, PROCESSED_ROOT, RAW_ROOT
-from data.utils import (get_meta_df, get_trial_index,
-get_trials, compute_label_thresholds)
+from data.utils import (get_meta_df, get_trial_index, 
+                        get_trials, compute_label_thresholds)
 
 
 def compute_three_class_label(file_path):
@@ -31,29 +31,30 @@ def compute_binary_label(file_path):
     return meta_df.loc[index]['RESP_4W']
 
 
-def create_labels(label_func, pickle_name, input_path=PROCESSED_ROOT,
-                  output_path=LABELED_ROOT):
+def create_labels(input_path=PROCESSED_ROOT, output_path=LABELED_ROOT):
     logging.info('Creating labels...')
-    trials = get_trials(RAW_ROOT)
-    labels_df = pd.DataFrame(columns=['label'], index=trials)
+    idxs = pd.MultiIndex.from_product([list(range(1, 134)), ['a', 'b']],
+                                      names=['patient', 'trial'])
+    labels_df = pd.DataFrame(columns=['label_d', 'label_r', 'label_ba'], index=idxs)
     for file_name in os.listdir(input_path):
         if not file_name.endswith('.fif'):
             logging.info('Skipping file %s' % file_name)
             continue
         file_path = os.path.join(input_path, file_name)
-        _, _, trial = get_trial_index(file_name)
-        labels_df.loc[trial]['label'] = label_func(file_path)
+        trial, index, _ = get_trial_index(file_name)
+        labels_df.loc[(index, trial)]['label_d'] = compute_three_class_label(file_path)
+        labels_df.loc[(index, trial)]['label_r'] = compute_binary_label(file_path)
+        labels_df.loc[(index, trial)]['label_ba'] = 0 if trial == 'a' else 1
     logging.info('The resulting data: \n{}'.format(
         labels_df.describe()))
     logging.info('Saving label data as pickle...')
-    labels_df.to_pickle(os.path.join(output_path, pickle_name))
+    labels_df.to_pickle(os.path.join(output_path, 'labels.pickle'))
 
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
 
-    create_labels(compute_three_class_label, 'labels_depressed.pickle')
-    create_labels(compute_binary_label, 'labels_response.pickle')
+    create_labels()
 
 
 if __name__ == '__main__':
